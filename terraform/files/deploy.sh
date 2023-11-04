b17372d44eece64a6ab2b5d2e315efa7ea492b6f
Iskander Zhienaliev <sparkinikwow@gmail.com>
2023-11-04 17:55:12 +0100

    Terraform 1

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+APP_DIR=${1:-$HOME}
+until sudo apt install -y git 2>&1; do
+        sleep 1
+done
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
